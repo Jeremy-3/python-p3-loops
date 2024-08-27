8cfe0b680186464b1cbdf0b9a0c0f5ca894a3b17
+ <+>
2024-08-27 15:23:34 +0300

    test done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+        if counter == 0:
+            print("Happy New Year!")
     # code goes here!
-    pass
+    
 
 def square_integers(int_list):
+    for num in int_list:
+        print(num ** 2)
+        
+    return [num ** 2 for num in int_list]    
+         
     # code goes here!
-    pass
+   
 
 def fizzbuzz():
+    i = 0
+    while(i < 100):
+        i += 1
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
     # code goes here!
     pass
